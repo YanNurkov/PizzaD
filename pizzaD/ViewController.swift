1669883e6cbfa389b15dc9c12aae90a613f8b41d
YanNurkov <85751034+YanNurkov@users.noreply.github.com>
2022-10-13 22:21:25 +0300

    Initial Commit

---
+++ pizzaD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pizzaD
+//
+//  Created by Ян Нурков on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
